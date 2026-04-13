85ffd10a15b6d8398d9a5ccf98ffbad02195f5dd
Ludwing <ludwingytx@gmail.com>
2026-04-13 17:09:35 -0400

    update

--- app/layout.tsx
+++ app/layout.tsx
@@ -34,7 +34,6 @@ export default function RootLayout({
           defaultTheme="system"
           enableSystem
           disableTransitionOnChange
-          suppressHydrationWarning
         >
           <TooltipProvider>{children}</TooltipProvider>
           <Toaster />
--- components/theme-provider.tsx
+++ components/theme-provider.tsx
@@ -1,11 +1,13 @@
 "use client"
 
 import * as React from "react"
-import { ThemeProvider as NextThemesProvider } from "next-themes"
+import dynamic from "next/dynamic"
 
-export function ThemeProvider({
-  children,
-  ...props
-}: React.ComponentProps<typeof NextThemesProvider>) {
+const NextThemesProvider = dynamic(
+  () => import("next-themes").then((e) => e.ThemeProvider),
+  { ssr: false }
+)
+
+export function ThemeProvider({ children, ...props }: any) {
   return <NextThemesProvider {...props}>{children}</NextThemesProvider>
 }
